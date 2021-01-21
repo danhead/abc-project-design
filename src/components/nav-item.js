8f975d6d400d2aef329b4143a426ff42fc2d9aa8
Dan Head <me@danielhead.com>
2021-01-21 22:07:00 +0000

    fix(nav): increase speed

--- src/components/nav-item.js
+++ src/components/nav-item.js
@@ -32,7 +32,7 @@ const ListItem = styled.li`
     transform: ${props =>
       props.open ? `translate3d(0, 0, 0)` : `translate3d(-100%, 0, 0)`};
     transition: transform 0.3s ease-in-out
-      calc(0.3s + (${props => props.index} * 0.1s));
+      calc(0.15s + (${props => props.index} * 0.1s));
   }
 `;
 
