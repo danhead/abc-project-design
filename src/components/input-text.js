6f63e99a3cf5e4e9c5cda4b9a2280b2470d0ff87
Dan Head <me@danielhead.com>
2021-02-02 23:41:35 +0000

    fat(input-text): focus state

--- src/components/input-text.js
+++ src/components/input-text.js
@@ -6,6 +6,15 @@ const Input = styled.input`
   margin-bottom: var(--spacing-y-sm);
   padding: var(--spacing-y-xs) var(--spacing-x-sm);
   border: 1px solid var(--color-dark);
+
+  &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px var(--color-dark);
+  }
+
+  &:focus:not(:focus-visible) {
+    box-shadow: none;
+  }
 `;
 
 export default function InputText({
